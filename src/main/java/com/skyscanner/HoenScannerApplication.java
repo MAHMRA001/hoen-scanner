222a5dfc472c7914f1b595801b0a90ad00044b7d
mahmoud mraiwa <151856858+MAHMRA001@users.noreply.github.com>
2026-08-27 15:04:41 +0000

    Create search microservice

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,28 +1,64 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 
-public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(final String[] args) throws Exception {
+public class HoenScannerApplication
+        extends Application<HoenScannerConfiguration> {
+
+    public static void main(String[] args) throws Exception {
         new HoenScannerApplication().run(args);
     }
 
     @Override
     public String getName() {
-        return "hoen-scanner";
+        return "Hoen Scanner";
     }
 
     @Override
-    public void initialize(final Bootstrap<HoenScannerConfiguration> bootstrap) {
-
+    public void initialize(
+            final Bootstrap<HoenScannerConfiguration> bootstrap) {
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(
+            final HoenScannerConfiguration configuration,
+            final Environment environment) throws Exception {
 
+        ObjectMapper objectMapper = environment.getObjectMapper();
+        List<SearchResult> searchResults = new ArrayList<>();
+
+        try (
+                InputStream rentalCars = getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("rental_cars.json");
+
+                InputStream hotels = getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("hotels.json")
+        ) {
+            searchResults.addAll(objectMapper.readValue(
+                    rentalCars,
+                    new TypeReference<List<SearchResult>>() {
+                    }
+            ));
+
+            searchResults.addAll(objectMapper.readValue(
+                    hotels,
+                    new TypeReference<List<SearchResult>>() {
+                    }
+            ));
+        }
+
+        environment.jersey().register(
+                new SearchResource(searchResults)
+        );
     }
-
 }
